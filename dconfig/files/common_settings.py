d31c03abf120e3441ed7e6a1a32cb90dd4c694c1
shinznatkid <shinznatkid@gmail.com>
2015-05-24 12:13:20 +0700

    version 0.2.0 - re-implement configs file from secrets.json to configs.py - include raven

--- dconfig/files/common_settings.py
+++ dconfig/files/common_settings.py
@@ -1,22 +1,21 @@
 from utils.misc import import_app
 from pathlib import Path
-import json
+
+try:
+    import configs
+except ImportError:
+    configs = {}
 
 
 BASE = Path(__file__).resolve().parent.parent
-try:
-    with (BASE / 'secrets.json').open() as handle:
-        SECRETS = json.loads(handle.read())
-except IOError:
-    SECRETS = {}
 
-SECRET_KEY = SECRETS.get('secret_key', '{SECRET_KEY}')
-PRODUCTION = SECRETS.get('production', False)
+SECRET_KEY = getattr(configs, 'SECRET_KEY', '+y01%7#9aipmcca171@(%%3i0v#mi(f32&a-(+r0=w_i7mj2yk')
+PRODUCTION = getattr(configs, 'PRODUCTION', False)
 
 DEBUG = not PRODUCTION
 TEMPLATE_DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = [SECRETS.get('allowed_hosts')]
+ALLOWED_HOSTS = getattr(configs, 'ALLOWED_HOSTS', [])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -31,21 +30,22 @@ INSTALLED_APPS = [
 ROOT_URLCONF = '{DJANGO_PROJECT}.urls'
 WSGI_APPLICATION = '{DJANGO_PROJECT}.wsgi.application'
 
-if not SECRETS.get('engine') or SECRETS.get('engine') == 'sqlite3':
+ENGINE = getattr(configs, 'ENGINE', None)
+if not ENGINE or ENGINE == 'sqlite3':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': SECRETS.get('db_name', str(BASE / 'db.sqlite3')),
+            'NAME': getattr(configs, 'DB_NAME', str(BASE / 'db.sqlite3')),
         }
     }
-elif SECRETS.get('engine') == 'mysql':
+elif ENGINE == 'mysql':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': SECRETS.get('db_name', 'dbname'),
-            'USER': SECRETS.get('db_user', 'root'),
-            'PASSWORD': SECRETS.get('db_password', 'password'),
-            'HOST': SECRETS.get('db_host', '127.0.0.1'),
+            'NAME': getattr(configs, 'DB_NAME', 'dbname'),
+            'USER': getattr(configs, 'DB_USER', 'root'),
+            'PASSWORD': getattr(configs, 'DB_PASSWORD', 'password'),
+            'HOST': getattr(configs, 'DB_HOST', '127.0.0.1'),
         }
     }
 
@@ -98,3 +98,76 @@ if import_app('picker', INSTALLED_APPS):
     )
 
 import_app('dconfig', INSTALLED_APPS)  # Try import dconfig it self
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARN',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+    },
+}
+
+RAVEN_DSN = getattr(configs, 'RAVEN_DSN', False)
+if RAVEN_DSN:
+    import raven
+    INSTALLED_APPS += [
+        'raven.contrib.django.raven_compat',
+    ]
+
+    RAVEN_CONFIG = {
+        'dsn': RAVEN_DSN,
+        'release': raven.fetch_git_sha(str(BASE)),
+    }
+
+    LOGGING['formatters']['verbose']  = {
+        'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+    }
+    LOGGING['handlers']['sentry'] = {
+        'level': 'ERROR',
+        'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+    }
+    LOGGING['root'] = {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    }
+    LOGGING['loggers']['raven'] = {
+        'level': 'DEBUG',
+        'handlers': ['console'],
+        'propagate': False,
+    }
+    LOGGING['loggers']['sentry.errors'] = {
+        'level': 'DEBUG',
+        'handlers': ['console'],
+        'propagate': False,
+    }
+    LOGGING['handlers']['console']['formatter'] = 'verbose'
+
+    MIDDLEWARE_CLASSES.append('raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware')
+    MIDDLEWARE_CLASSES.append('raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware')
