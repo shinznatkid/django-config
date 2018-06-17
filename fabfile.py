23abf7efe450e63685109a7fc76dc84290bd8e4a
Shinz Natkid <shinznatkid@gmail.com>
2018-06-17 08:01:06 +0700

    - update upload process

--- fabfile.py
+++ fabfile.py
@@ -2,4 +2,7 @@ from fabric.api import local
 
 
 def upload():
-    local('python setup.py bdist_wheel upload')
+    # local('python setup.py bdist_wheel upload')
+    local('rm -r ./dist')
+    local('python setup.py sdist bdist_wheel')
+    local('twine upload --repository-url https://upload.pypi.org/legacy/ dist/*')
